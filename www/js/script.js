91e924b88d891c60a2fa7cbced3bd9b3a61895bf
Chimitoh <ivanhotimich@yandex.ru>
2014-02-24 22:33:03 +0400

    24.02.2014
    допилены списки номеров вариантов

--- www/js/script.js
+++ www/js/script.js
@@ -12,11 +12,21 @@ function getXmlHttpRequest(){
 	}
 }
 
-function getValuesByField(fieldName){
+function removeChildren(node) {
+	var children = node.childNodes;
+	if (typeof children!="undefined")	while (children.length) node.removeChild(children[0]);
+}
+
+
+function getValuesByField(fieldName, fieldsValues){
 	var xmlHTTP = getXmlHttpRequest();
+	var strFieldsValues='&fieldsValues=';
+	if (typeof fieldsValues!="undefined") {
+	  strFieldsValues+=fieldsValues.join(',');
+	} else strFieldsValues=''; 
 	xmlHTTP.open("POST","php/getValuesByFieldName.php",false);
 	xmlHTTP.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xmlHTTP.send('fieldName='+fieldName);
+	xmlHTTP.send('fieldName='+fieldName+strFieldsValues);
 	if (xmlHTTP.responseText!=0) return xmlHTTP.responseText.split(',');		
 }
 
@@ -26,15 +36,16 @@ function setOption(select, item){
 	select.appendChild(option);
 }
 
-//берет из базы данных записи и пихает их в select c id='select'+fieldName
-function setValuesSelect(fieldName){
-  var mass = getValuesByField(fieldName);
+//берет из базы данных записи и пихает их в select c id=fieldName fieldsValues - массив со значениями полей для выборки поля fieldName
+function setValuesSelect(fieldName, fieldsValues){
+  var mass = getValuesByField(fieldName, fieldsValues);
   var select = document.getElementById(fieldName);
+	removeChildren(select);
 	for (var i=0; i<mass.length; i++) setOption(select, mass[i]);
 }
 
 function setValuesSelectProblems(){
   setValuesSelect('problem');
-	setValuesSelect('level');
-	setValuesSelect('variant');
+	setValuesSelect('level',[document.getElementById('problem').value]);
+	setValuesSelect('variant',[document.getElementById('problem').value,document.getElementById('level').value]);
 }
--- www/php/getValuesByFieldName.php
+++ www/php/getValuesByFieldName.php
@@ -1,14 +1,37 @@
 <?php
   //выбирает значения столбца из таблицы problems для вставки в <SELECT>
   include 'mySqlConnect.php';
-	// $_POST['fieldName']='problem';
-	if (($_POST['fieldName']=='problem')||($_POST['fieldName']=='level')||($_POST['fieldName']=='variant')){ 
-		$st=$db->query('SELECT '.$_POST['fieldName'].' FROM problems');
+	// $_POST['fieldName']='level';
+	// $_POST['fieldsValues']='2';
+	if (($_POST['fieldName']=='problem')||($_POST['fieldName']=='level')||($_POST['fieldName']=='variant')){
+		if (isset($_POST['fieldsValues'])) {
+			$where=' WHERE ';
+			$fieldsMass=explode(',',$_POST['fieldsValues']);
+			for ($i=0;$i<count($fieldsMass);$i++) {
+				switch ($i) {
+					case 0: {
+					  $param='problem';
+						$value=':p';
+					} break;
+					case 1: {
+					  $param='level';
+						$value=':l';
+					} break;
+				}
+				$where.=$param."=".$value.(($i<count($fieldsMass)-1)?' and ':'');
+			}
+		} else $where='';
+		$st=$db->prepare('SELECT DISTINCT '.$_POST['fieldName'].' FROM problems '.$where.' ORDER BY '.$_POST['fieldName'].' ASC');
+		if (isset($fieldsMass)) {
+			$st->bindValue(':p',$fieldsMass[0]);
+			if (count($fieldsMass)>1) $st->bindValue(':l',$fieldsMass[1]);
+		}
+		$st->execute();
 		if ($st) {
 		  $mass = array();
   		while ($a=$st->fetch(PDO::FETCH_ASSOC)) array_push($mass, $a[$_POST['fieldName']]);
 			echo implode(',', $mass);
 		}
 	}
-	// echo $_POST['fieldName'];
+	// echo $_POST['fieldValue'];
 ?>
